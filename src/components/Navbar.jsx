28f956f5081b5e06fc6969b041462f08d452fa07
Tomasz <tomasz.iwanicki@onet.pl>
2023-05-08 17:58:19 +0200

    Complete Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { styles, data, images } from '../constants'
+
+import { motion } from 'framer-motion'
+import { slideIn } from '../constants/motion'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  useEffect(() => {
+    function handleResize() {
+      setToggleMenu(false)
+    }
+
+    window.addEventListener('resize', handleResize)
+  }, [window.innerWidth, window.innerHeight])
+
+  return (
+    <nav className={`${styles.padding} flex justify-between items-center bg-primary relative`}>
+      <a href="/">
+        <p className="text-white text-[18px]">
+          Tomasz <span className="text-secondary">Iwanicki</span>
+        </p>
+      </a>
+      <ul className="md:flex hidden lg:gap-12 gap-6">
+        {data.navLinks.map((link) => (
+          <a href={link.path} key={link.title}>
+            <li className="text-white hover:text-secondary">{link.title}</li>
+          </a>
+        ))}
+      </ul>
+      <img src={images.iconHamburger} alt="menu" className={`${toggleMenu ? 'hidden' : 'flex'} md:hidden hover:cursor-pointer`} onClick={() => setToggleMenu((prev) => !prev)} />
+
+      {toggleMenu && (
+        <motion.div initial={{ opacity: 0, x: '-100%' }} animate={{ opacity: 1, x: 0 }} className="absolute right-0 top-0 h-[100vh] nav-mobile-bg w-3/5">
+          <div className="flex flex-col p-6">
+            <div className="flex justify-end">
+              <img src={images.iconClose} alt="close" onClick={() => setToggleMenu((prev) => !prev)} className="w-[20px] h-[20px]" />
+            </div>
+            <ul className="flex flex-col gap-12 mt-12">
+              {data.navLinks.map((link) => (
+                <a href={link.path} key={link.title}>
+                  <li className="text-white hover:text-secondary" onClick={() => setToggleMenu(false)}>
+                    {link.title}
+                  </li>
+                </a>
+              ))}
+            </ul>
+          </div>
+        </motion.div>
+      )}
+    </nav>
+  )
 }
 
 export default Navbar
