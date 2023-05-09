041f7ca02c7bc8efa8c772fdcfb6080f39c5a8b4
Tomasz <tomasz.iwanicki@onet.pl>
2023-05-09 18:58:40 +0200

    Complete Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { styles, images } from '../constants'
+import { motion } from 'framer-motion'
 
 const Hero = () => {
-  return <div>Hero</div>
+  return (
+    <section className={`${styles.padding} flex lg:flex-row flex-col-reverse justify-between bg-darkGray`}>
+      <motion.div initial={{ x: '-30%' }} animate={{ x: 0 }} transition={{ duration: 0.9 }} className="flex flex-col lg:items-start items-center my-4 gap-6">
+        <div className="flex flex-col lg:items-start items-center">
+          <p className="text-white font-[500] text-[24px]">
+            Hello, <span className="text-secondary ml-2">I'm</span>
+          </p>
+          <p className="text-secondary md:text-[40px] text-[32px] font-[600]">Tomasz Iwanicki</p>
+          <p className="text-white font-[500] text-[24px]">Front-end Developer</p>
+        </div>
+        <p className="text-lightGray text-[14px] lg:w-1/2 w-full lg:text-start text-center">I create web pages with UI/UX user interface. With knowledge in web development, I offer the best projects and designs.</p>
+        <a href="#contact">
+          <button className="w-[120px] bg-secondary hover:bg-white border-none text-white hover:text-secondary px-4 py-3 rounded-md mt-2">Let's Talk</button>
+        </a>
+      </motion.div>
+      <motion.div initial={{ x: '30%' }} animate={{ x: 0 }} transition={{ duration: 0.9 }} className="xl:w-1/2 w-full lg:mb-0 my-6">
+        <img src={images.vsCodeImage} alt="vscode" className="rounded-lg lg:min-h-[350px]" />
+      </motion.div>
+    </section>
+  )
 }
 
 export default Hero
