3c55795d7a6143e169636cdd259e3150d06390fe
Tomasz <tomasz.iwanicki@onet.pl>
2023-05-11 19:00:50 +0200

    Complete footer section of the page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { styles } from '../constants'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className={`${styles.paddingFooter} flex md:flex-row flex-col gap-12 items-center justify-between bg-primary`}>
+      <div>
+        <a href="/">
+          <p className="text-white lg:text-[24px] text-[18px]">
+            Tomasz <span className="text-secondary">Iwanicki</span>
+          </p>
+        </a>
+        <p className="text-white">Front-end Developer</p>
+      </div>
+      <div className="md:text-start text-center">
+        <p className="text-lightGray lg:block hidden">
+          <span className="text-white font-bold">Contact me</span> through the form showed above or:
+        </p>
+        <p className="text-lightGray">
+          Tel: <span className="font-bold text-white ml-2"> 794 478 594</span>
+        </p>
+        <p className="text-lightGray">
+          Email:<span className="font-bold text-white ml-2"> tomasz.iwanicki@onet.pl</span>
+        </p>
+      </div>
+      <p className="text-lightGray text-[14px]">Copyright &copy; All Rights Reserved.</p>
+    </footer>
+  )
 }
 
 export default Footer
